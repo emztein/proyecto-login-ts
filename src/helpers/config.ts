12506056cf344e05e2e1785d37cce791d1da62be
Andres Arias <fabian.arias@gorillalogic.com>
2024-05-17 10:15:42 -0500

    include doppler and joi for env vars vaidation

---
+++ src/helpers/config.ts
@@ -0,0 +1,17 @@
+import * as Joi from 'joi';
+
+//definir schema de validacion usando Joi
+const envSchema = Joi.object({
+  JWT_SECRET: Joi.string().required(),
+}).options({ allowUnknown: true });;
+
+//validar las variables de entorno
+const { error, value: envVars } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Configuracion de variables de entorno invalida: ${error.message}`)
+}
+
+export const CONFIG = {
+  JWT: envVars.JWT_SECRET
+}
